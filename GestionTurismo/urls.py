37827a045c83052ea8a18efcd49352ca6b243251
Jordan <aimbox_prox@hotmail.cl>
2020-09-14 03:55:50 -0300

    detalles

--- GestionTurismo/urls.py
+++ GestionTurismo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'), 
 ]
 
 urlpatterns += [
--- GestionTurismo/views.py
+++ GestionTurismo/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     return render(request, 'index.html', context={},)
+
+def login(request):
+    return render(request, 'login.html', {})
